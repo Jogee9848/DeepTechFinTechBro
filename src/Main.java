e2b0c05eb039ba0616caf4c2be1f6efbc81d2f1a
Jogee9848 <jogeenaidupeddakota@gmail.com>
2026-01-22 14:12:44 +0530

    First Commit - Mynewproject

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Hello and welcome!");
+
+        // Loop from 1 to 5 and print each value
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
